cee466161f06f50ebc61d1e56ceb4ae28fb2e681
aw_santo <sanghormode2@gmail.com>
2022-10-22 09:18:12 +0530

    installed cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 3333;
 const router = require("./routes/routes");
 app.use("/", router);
+app.use(cors());
 
 const connectDB = require("./db");
 connectDB();
